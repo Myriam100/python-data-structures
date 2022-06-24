9d11573dc8492d482f1fa305bbe54f986b797266
Myriam100 <108014031+Myriam100@users.noreply.github.com>
2022-06-24 07:15:03 -0400

    Add files via upload

---
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -0,0 +1,16 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    # separate = list(phrase)
+    for to_swap in phrase:
+        return to_swap.swapcase()
